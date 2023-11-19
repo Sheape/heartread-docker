4b1ef227fc9f8f8024a7f0091f8da40fd0fe022e
Sheape <86521166+Sheape@users.noreply.github.com>
2023-11-19 19:31:28 +0800

    initial commit

---
+++ app/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.model.model import ecg_predict
+from app.model.model import __version__ as model_version
+
+app = FastAPI()
+
+
+class FileInput(BaseModel):
+    filename: str
+
+
+class PredictionOut(BaseModel):
+    classifications: dict
+
+@app.get("/")
+def home():
+    return {"status": "OK", "model_version": model_version}
+
+@app.post("/predict/ecg")
+def ecg_classify(payload: FileInput):
+    classifications = ecg_predict(payload.filename)
+    return classifications
---
+++ app/model/model.py
@@ -0,0 +1,69 @@
+from keras.models import load_model
+from scipy.io import loadmat
+from scipy.signal import filtfilt, butter
+import numpy as np
+import os
+import ecg_plot
+from pathlib import Path
+
+__version__ = '0.1.0'
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+model = load_model(f"{BASE_DIR}/ecg_model_sigmoid-{__version__}.h5")
+labels = [
+    'Pacing Rhythm',
+    'Axis left shift',
+    'Axis right shift',
+    'Left ventricular hypertrophy',
+    'S-T changes',
+    'Right bundle branch block',
+    'T wave opposite',
+    'Counterclockwise vectorcardiographic loop',
+    'Left ventricle hypertrophy',
+    'Atrial Fibrillation',
+    'Atrial Flutter',
+    'Abnormal Q wave',
+    'ST extension',
+    'T wave Change',
+    'Lower voltage QRS in all lead',
+    '1 Degree atrioventricular block',
+    'Atrial premature beats',
+    'Poor R wave progression',
+    'Sinus Bradycardia',
+    'Supraventricular Tachycardia',
+    'Sinus Rhythm',
+    'Sinus Tachycardia',
+    'Premature ventricular contractions',
+    'Sinus Irregularity',
+    'ST-T Change',
+    'ST drop down',
+    'Intraventricular block',
+    'Complete right bundle branch block'
+]
+
+
+def load_mat(filename):
+    x = loadmat(filename)
+    data = np.asarray(x['val'], dtype=np.float64)
+    Wn = 0.1
+    b, a = butter(4, Wn, 'low', analog=False)
+    smooth_ecg = filtfilt(b, a, data)
+
+    return smooth_ecg
+
+
+def plot_ecg(filename):
+    ecg_data = load_mat(filename)
+    filename_only, _ = os.path.splitext(filename)
+    ecg_plot.plot_12(ecg_data/1000, sample_rate=500, title="12-Lead ECG Graph")
+    ecg_plot.save_as_svg(filename_only)
+
+
+def ecg_predict(filename):
+    ecg = load_mat(filename)
+    reshaped_ecg = ecg.reshape(1, 5000, 12)
+    prediction = model.predict(x=reshaped_ecg)
+    top_5 = sorted(prediction[0].tolist(), reverse=True)[:5]
+    top_5_dict = {prediction[0].tolist().index(value): value for value in top_5}
+    result_dict = {labels[k]: v for k, v in top_5_dict.items()}
+    return result_dict
